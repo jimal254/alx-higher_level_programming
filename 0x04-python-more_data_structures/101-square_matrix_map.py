25b1a36dbdb2d6106521430ab7e3b2c78ce23bf4
jimmy <jimmymwas@gmail.com>
2023-12-08 05:51:49 -0800

    More Data Structures

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
-70% of storage used … If you run out, you won't have enough storage to create, edit, and upload files. Get 100 GB of storage for Ksh 250.00 Ksh 60.00/month for 3 months.
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,6 @@
-[200~70% of storage used … If you run out, you won't have enough storage to create, edit, and upload files. Get 100 GB of storage for Ksh 250.00 Ksh 60.00/month for 3 months.
-        #!/usr/bin/python3
-        def complex_delete(a_dictionary, value):
-            list_keys = list(a_dictionary.keys())
-
-                for value_dic in list_keys:
-                        if value == a_dictionary.get(value_dic):
-                                    del a_dictionary[value_dic]
-
-                                        return (a_dictionary)
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for key in list(a_dictionary):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
