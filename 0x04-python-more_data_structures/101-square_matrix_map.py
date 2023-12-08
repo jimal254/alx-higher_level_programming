a26eb2d2c803804deefd7fa7b8b1dde1a54f29d3
jimmy <jimmymwas@gmail.com>
2023-12-08 05:44:53 -0800

    More Data Structures

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,4 @@
+70% of storage used … If you run out, you won't have enough storage to create, edit, and upload files. Get 100 GB of storage for Ksh 250.00 Ksh 60.00/month for 3 months.
+#!/usr/bin/python3
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+[200~70% of storage used … If you run out, you won't have enough storage to create, edit, and upload files. Get 100 GB of storage for Ksh 250.00 Ksh 60.00/month for 3 months.
+        #!/usr/bin/python3
+        def complex_delete(a_dictionary, value):
+            list_keys = list(a_dictionary.keys())
+
+                for value_dic in list_keys:
+                        if value == a_dictionary.get(value_dic):
+                                    del a_dictionary[value_dic]
+
+                                        return (a_dictionary)
