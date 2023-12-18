c541d57da99d0aefc16d9ff657c0b5d9cf7caa8f
jimmy maina <jimmymwas@gmail.com>
2023-12-18 18:15:54 +0000

    Python - Exceptions

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,4 +1,6 @@
-uth: Type Your Name Here
+/*
+ * File: 103-python.c
+ * Auth: Type Your Name Here
  */
 
 #include <Python.h>
@@ -13,32 +15,33 @@ void print_python_float(PyObject *p);
  */
 void print_python_list(PyObject *p)
 {
-    Py_ssize_t size, alloc, i;
-    const char *type;
-    PyListObject *list;
+        Py_ssize_t size, alloc, i;
+        const char *type;
+        PyListObject *list = (PyListObject *)p;
+        PyVarObject *var = (PyVarObject *)p;
 
-    if (!PyList_Check(p))
-    {
-        fprintf(stderr, "[*] Python list info\n  [ERROR] Invalid List Object\n");
-        return;
-    }
+        size = var->ob_size;
+        alloc = list->allocated;
 
-    list = (PyListObject *)p;
-    size = PyList_Size(p);
-    alloc = list->allocated;
+        fflush(stdout);
 
-    printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", alloc);
+        printf("[*] Python list info\n");
+        if (strcmp(p->ob_type->tp_name, "list") != 0)
+        {
+                printf("  [ERROR] Invalid List Object\n");
+                return;
+        }
 
-    for (i = 0; i < size; i++)
-    {
-        type = Py_TYPE(list->ob_item[i])->tp_name;
-        printf("Element %ld: %s\n", i, type);
+        printf("[*] Size of the Python List = %ld\n", size);
+        printf("[*] Allocated = %ld\n", alloc);
 
-        if (strcmp(type, "bytes") == 0)
-            print_python_bytes(list->ob_item[i]);
-        else if (strcmp(type, "float") == 0)
-            print_python_float(list->ob_item[i]);
-    }
+        for (i = 0; i < size; i++)
+        {
+                type = list->ob_item[i]->ob_type->tp_name;
+                printf("Element %ld: %s\n", i, type);
+                if (strcmp(type, "bytes") == 0)
+                        print_python_bytes(list->ob_item[i]);
+                else if (strcmp(type, "float") == 0)
+                        print_python_float(list->ob_item[i]);
+        }
 }
