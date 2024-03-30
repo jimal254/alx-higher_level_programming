8f03b922d89dd5a7ac52e159acb1a2a709d9c17b
jimmy maina <jimmymwas@gmail.com>
2024-03-30 10:08:40 +0000

    0x10. Python - Network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Send a POST request to 0.0.0.0:5000/catch_me with curl
-curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+# Send a PUT request to 0.0.0.0:5000/catch_me with curl and make the server respond with "You got me!"
+curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
